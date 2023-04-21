88defa360ad8c1474df29eaa455bbb0f6523993c
ViniVanC <vincentvan0302@gmail.com>
2023-04-21 18:57:23 +0300

    created db.js

---
+++ src/db.js
@@ -0,0 +1,50 @@
+// тут мають бути усі списки
+
+export const headerNavList = [
+  {
+    title: "Home",
+    href: "#",
+    children: [],
+  },
+  {
+    title: "About",
+    href: "#",
+    children: [],
+  },
+  {
+    title: "Pages",
+    href: "#",
+    children: [
+      {
+        title: "Page 1",
+        href: "#",
+        children: [],
+      },
+      {
+        title: "Page 2",
+        href: "#",
+        children: [],
+      },
+      {
+        title: "Page 3",
+        href: "#",
+        children: [],
+      },
+    ],
+  },
+  {
+    title: "Shop",
+    href: "#",
+    children: [],
+  },
+  {
+    title: "Projects",
+    href: "#",
+    children: [],
+  },
+  {
+    title: "News",
+    href: "#",
+    children: [],
+  },
+];
