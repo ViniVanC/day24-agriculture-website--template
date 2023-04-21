bd6a55ea1f077a5f35fbb3b9c95e7c24f2208965
ViniVanC <vincentvan0302@gmail.com>
2023-04-21 20:42:45 +0300

    added Header to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Header } from "./components/Header/Header";
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
