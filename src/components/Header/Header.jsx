77e7ee94fcffc9554f0246867c72251af9a91cc2
ViniVanC <vincentvan0302@gmail.com>
2023-04-21 18:56:41 +0300

    created Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Container } from "../Container";
+import { Logo } from "../Logo/Logo";
+import { Navbar } from "../Navbar/Navbar";
+import { headerNavList } from "../../db";
+import { Search } from "../Search/Search";
+import { GrCart } from "react-icons/gr";
+
+export const Header = () => {
+  const [cartNum] = useState(0);
+
+  return (
+    <header className="header">
+      <Container>
+        <div className="header__inner">
+          <Logo className={"header__logo"} />
+          <Navbar navbarList={headerNavList} />
+          <div className="header__end">
+            <Search />
+            <div className="header__cart header-cart">
+              <button className="header-cart__button">
+                <GrCart />
+              </button>
+              <span className="header-cart__num">Cart ({cartNum})</span>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
